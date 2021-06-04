46ad3796b235ab8ccb89800219907c13d3636639
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-06-04 13:30:23 +0200

    Rocket list cards

--- beyond-astrospace/src/App.js
+++ beyond-astrospace/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import RocketList from "./components/RocketList";
+import LaunchList from "./components/LaunchList";
 
 function App() {
   return (
     <div>
       <RocketList />
+      {/* <LaunchList /> */}
     </div>
   );
 }
