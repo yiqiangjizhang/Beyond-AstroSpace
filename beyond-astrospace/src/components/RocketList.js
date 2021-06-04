681d8af6a5f7beffdd72164bcc5122dc8c19ba7e
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-06-04 11:05:18 +0200

    RockeList update

--- beyond-astrospace/src/components/RocketList.js
+++ beyond-astrospace/src/components/RocketList.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./RocketList.css";
+
+const Rocket = (props) => {
+  return <div className="rocket">{props.rocket.name}</div>;
+};
 
 function RocketList() {
   // Create state for list of rockets
   const [rocket_list, set_rocket_list] = useState(null);
+
+  // Load rockets function
+  const loadRockets = async () => {
+    // Get the response from the server from url
+    const response = await fetch(`https://api.spacexdata.com/v3/rockets`);
+
+    // Convert to jso5
+    const json = await response.json();
+
+    // Set list to json object
+    set_rocket_list(json);
+  };
+
+  // Update action, only call once
+  useEffect(() => {
+    loadRockets(); // Call function
+  }, []);
 
   // If loading, then display loading message
   if (rocket_list === null) {
     return <div>Loading...</div>;
   }
 
-  return <div>Adios</div>;
+  // For every rocket in rocket list, pass rocket id and rocket object to rocket list
+  return (
+    //     <div>
+    // {RocketList.map((rocket_item) => (
+    // <Rocket key={rocket_item.id} rocket={rocket_item} />
+    // ))}
+    // </div>
+    <div>Hola</div>
+  );
 }
 
 export default RocketList;
